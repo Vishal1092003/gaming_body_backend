3447bb119de987e69badfd471477739f9d118031
Roushan225 <roushan2029@gmail.com>
2026-05-31 15:26:35 +0530

    added admin bethistory page routes

--- routes/betRoutes.js
+++ routes/betRoutes.js
@@ -5,13 +5,16 @@ const {
   getLosses,
   getSummary,
   createBet,
+  getAdminHistory,
 } = require('../controllers/betController');
 const { authenticate } = require('../middleware/authMiddleware');
+const { requireAdmin } = require('../middleware/adminMiddleware');
 
 const router = express.Router();
 
 router.use(authenticate);
 router.get('/history', getHistory);
+router.get('/admin/history', requireAdmin, getAdminHistory);
 router.get('/wins', getWins);
 router.get('/losses', getLosses);
 router.get('/summary', getSummary);
